63160bb0d52acde305a455e78e90e14cfbbf2ed8
Kelechi Uche <uchekelechy@gmail.com>
2020-06-27 08:47:26 +0100

    commit 27/07

--- Akirs.client/DL/Akirs.Designer.cs
+++ Akirs.client/DL/Akirs.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\LENOVO\Desktop\Akirs-master\Akirs.client\DL\Akirs.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\KCY IETECH\Desktop\Akirs-master\Akirs.client\DL\Akirs.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
